70b96c256ed6b84227462e7958091d85c8d1a1dc
dianguas <dianguas@unirioja.es>
2026-04-20 17:07:29 +0200

    Clase 20/04 finalizada

--- src/main/java/server/RequestData.java
+++ src/main/java/server/RequestData.java
@@ -1,4 +1,26 @@
 package server;
 
+import model.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class RequestData {
+    private Map<String,Integer>  cells=new HashMap<String,Integer>();
+
+    RequestData(String immobileCell, int numImm, String mobileCell, int numMobile, String reproductiveCell, int numRepro){
+        cells.put(immobileCell,numImm);
+        cells.put(mobileCell,numMobile);
+        cells.put(reproductiveCell,numRepro);
+    }
+
+    public Map<String, Integer> getCells() {
+        return cells;
+    }
+
+    public void setCells(Map<String, Integer> cells) {
+        this.cells = cells;
+    }
 }

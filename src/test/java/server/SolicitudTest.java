d45b590798cb3f65d87b247cca1bb2455fa3ed64
huarandi <huarandi@unirioja.es>
2026-04-20 16:46:15 +0200

    test Solicitud

--- src/test/java/server/SolicitudTest.java
+++ src/test/java/server/SolicitudTest.java
@@ -1,12 +1,39 @@
 package server;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import server.mock.RequestManagerMock;
 
 public class SolicitudTest {
 
     private Solicitud solicitud;
+
+    private final String USER = "usuario";
+    private static String REQUEST;
+    private static String BAD_REQUEST = "";
+
+    @BeforeClass
+    public static void setUpClass() {
+        JsonObject req = new JsonObject();
+        JsonArray cant = new JsonArray();
+        cant.add(10);
+        cant.add(20);
+        cant.add(30);
+        req.add("cantidadesIniciales", cant);
+
+        JsonArray ents = new JsonArray();
+        ents.add("dynamic");
+        ents.add("static");
+        ents.add("reproductive");
+        req.add("nombreEntidades", ents);
+
+        REQUEST = req.toString();
+    }
 
     @Before
     public void setUp() {
@@ -15,7 +42,30 @@ public class SolicitudTest {
     }
 
     @Test
-    public void postSolicitudSolicitar(){
+    public void postSolicitudSolicitarBadRequest(){
+        String strRes = solicitud.postSolicitudSolicitar(USER, BAD_REQUEST);
 
+        JsonObject res = JsonParser.parseString(strRes).getAsJsonObject();
+        Assert.assertNotNull(res);
+        Assert.assertNotNull(res.get("type").getAsString());
+        Assert.assertNotNull(res.get("title").getAsString());
+        Assert.assertNotNull(res.get("status").getAsInt());
+        Assert.assertNotNull(res.get("detail").getAsString());
+        Assert.assertNotNull(res.get("instance").getAsString());
+        Assert.assertNotNull(res.get("additionalProp1").getAsString());
+        Assert.assertNotNull(res.get("additionalProp2").getAsString());
+        Assert.assertNotNull(res.get("additionalProp3").getAsString());
+    }
+
+    @Test
+    public void postSolicitudSolicitarGoodRequest(){
+        String strRes = solicitud.postSolicitudSolicitar(USER, REQUEST);
+
+        JsonObject res = JsonParser.parseString(strRes).getAsJsonObject();
+        Assert.assertNotNull(res);
+        Assert.assertNotNull(res.get("done").getAsBoolean());
+        Assert.assertNotNull(res.get("tokenSolicitud").getAsInt());
+        Assert.assertNotNull(res.get("errormessage").getAsString());
+        Assert.assertNotNull(res.get("data").getAsBoolean());
     }
 }
